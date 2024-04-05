1326f01be76eec48f4f203e080288492786bbd79
jhu vitoria <jhuliavitoria226@gmail.com>
2024-04-05 14:08:59 -0300

    Resolve o problema que o exercício estava pedindo.

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Musica
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Atlantis - Seafret");
+            Console.ResetColor();
+            Console.WriteLine();  
+
+            Console.WriteLine("The birds have left their trees");
+            Console.WriteLine("The light pours onto me");
+            Console.WriteLine("I can feel you lying there all on your own");
+            Console.WriteLine("We got here the hard way");
+            Console.WriteLine("All those words that we exchange");
+            Console.WriteLine("Is it any wonder things get broke?");
+            Console.WriteLine();
+            Console.WriteLine("'Cause in my heart and in my head");
+            Console.WriteLine("I'll never take back the things I said");
+            Console.WriteLine("So high above, I feel it coming down");
+            Console.WriteLine("She said, in my heart and in my head");
+            Console.WriteLine("Tell me why this has to end");
+            Console.WriteLine("Oh, no, oh, no");
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("I can't save us, my Atlantis, we fall");
+            Console.WriteLine("We built this town on shaky ground");
+            Console.WriteLine("I can't save us, my Atlantis, oh, no");
+            Console.WriteLine("We built it up to pull it down"); 
+            Console.ResetColor();            
+            Console.WriteLine();
+
+            Console.WriteLine("Now all the birds have fled");
+            Console.WriteLine("The hurt just leaves me scared");
+            Console.WriteLine("Losing everything I've ever known");
+            Console.WriteLine("It's all become too much");
+            Console.WriteLine("Maybe I'm not built for love");
+            Console.WriteLine("If I knew that I could reach you, I would go");
+            Console.WriteLine();
+            Console.WriteLine("It's in my heart and in my head");
+            Console.WriteLine("You can't take back the things you said");
+            Console.WriteLine("So high above, I feel it coming down");
+            Console.WriteLine("She said, in my heart and in my head");
+            Console.WriteLine("Tell me why this has to end");
+            Console.WriteLine("Oh, no, oh, no");
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;  
+            Console.WriteLine("I can't save us, my Atlantis, we fall");
+            Console.WriteLine("We built this town on shaky ground");
+            Console.WriteLine("I can't save us, my Atlantis, oh, no");
+            Console.WriteLine("We built it up to pull it down");
+            Console.ResetColor();
+            Console.WriteLine();
+
+            Console.WriteLine("Yeah, we build it up and we build it up");
+            Console.WriteLine("Yeah, we build it up to pull it down");
+            Console.WriteLine("And we build it up and we build it up");
+            Console.WriteLine("And we build it up to pull it down");
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;           
+            Console.WriteLine("I can't save us, my Atlantis, we fall");
+            Console.WriteLine("We built this town on shaky ground");
+            Console.WriteLine("I can't save us, my Atlantis, oh, no");
+            Console.WriteLine("We built it up to pull it down");
+            Console.ResetColor();
+            Console.WriteLine();
+         }
+    }   
+}
